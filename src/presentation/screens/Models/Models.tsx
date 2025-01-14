b088d39f0a6a5f6d0adb834edd83886365c89a3f
Ceagah <carlosceagah@gmail.com>
2025-01-14 19:28:14 -0300

    feat: create initial screens files

---
+++ src/presentation/screens/Models/Models.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container } from "../../components";
+
+export default function Models() {
+  return (
+    <Container>
+      <Text> Models </Text>
+    </Container>
+  );
+}
