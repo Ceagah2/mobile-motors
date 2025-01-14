2d6836b796a05fe1900882326c18225b11e78820
Ceagah <carlosceagah@gmail.com>
2025-01-14 20:04:10 -0300

    style: change container and sizes token

--- src/presentation/components/Container/Container.styles.ts
+++ src/presentation/components/Container/Container.styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components/native'
 import { colors } from '../../theme'
 
-export const MainContainer = styled.View`
+export const MainContainer = styled.SafeAreaView`
   flex: 1 ;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   background-color: ${colors.background};
   padding: 0 15px;
--- src/presentation/theme/sizes.ts
+++ src/presentation/theme/sizes.ts
@@ -1,7 +1,7 @@
 export const sizes = { 
-  heading: "32px",
-  subHeading: "24px",
-  body: "16px",
-  button: "14px",
-  caption: "12px",
+  heading: 28,
+  subHeading: 18,
+  body: 16,
+  button: 14,
+  caption: 12,
 }
