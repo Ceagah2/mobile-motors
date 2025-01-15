85637024736856b713717343707a6a7314c11109
Ceagah <carlosceagah@gmail.com>
2025-01-15 08:40:53 -0300

    feat: implement expo google fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
+import { Poppins_300Light, Poppins_400Regular, Poppins_700Bold } from "@expo-google-fonts/poppins";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from "expo-font";
+import { ActivityIndicator } from "react-native";
 import Home from "./src/presentation/screens/Home/Home";
 import Login from "./src/presentation/screens/Login/Login";
 import Models from "./src/presentation/screens/Models/Models";
 
 export default function App() {
+  const fontsLoaded = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    Poppins_400Regular,
+    Poppins_300Light,
+    Poppins_700Bold
+  })
   const Stack = createNativeStackNavigator()
+
+  if(!fontsLoaded){
+    <ActivityIndicator />
+  }
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
