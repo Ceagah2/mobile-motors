33e96d34cef6b6498c3bc7d2946e87e958bf10d3
Ceagah <carlosceagah@gmail.com>
2025-01-15 08:42:09 -0300

    style: add margin on header

--- src/presentation/components/HeaderWithLogo/Header.styles.ts
+++ src/presentation/components/HeaderWithLogo/Header.styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { colors, sizes } from '../../theme';
+import { colors, fonts, sizes } from '../../theme';
 import { useResponsive } from './../../hooks/useResponsive';
 
 export const Header = styled.View`
@@ -7,6 +7,7 @@ export const Header = styled.View`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  margin-bottom: 16px;
 `
 export const Logo = styled.Image`
   width: 150px;
@@ -16,9 +17,11 @@ export const Logo = styled.Image`
 export const Title = styled.Text`
   color: ${colors.textPrimary};
   font-size: ${useResponsive(sizes.heading)};
+  font-family: ${fonts.heading};
 `
 export const SubTitle = styled.Text`
   color: ${colors.textSecondary};
   font-size: ${useResponsive(sizes.subHeading)};
+  font-family: ${fonts.heading};
   text-align: center;
 `
--- src/presentation/theme/fonts.ts
+++ src/presentation/theme/fonts.ts
@@ -1,5 +1,6 @@
 export const fonts = {
   heading: "Roboto_700Bold",
   body: "Roboto_400Regular",
-  button: "Roboto_500Medium",
+  button: "Poppins_400Regular",
+  caption: "Poppins_300Light"
 }
